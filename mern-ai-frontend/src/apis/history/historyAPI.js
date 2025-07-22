bfca24cec0cc6ab681385b1aa31da26143330ebd
YA-shiKa <myashika2005@gmail.com>
2025-07-22 16:48:13 +0530

    fix

--- mern-ai-frontend/src/apis/history/historyAPI.js
+++ mern-ai-frontend/src/apis/history/historyAPI.js
@@ -1,8 +1,10 @@
-import axios from "axios";
 
+
+import axios from "axios";
+const API = import.meta.env.VITE_API_BASE_URL;
 export const getContentHistoryAPI = async () => {
   const response = await axios.get("http://localhost:8090/api/v1/history", {
     withCredentials: true,
   });
   return response?.data;
-};
+};  
