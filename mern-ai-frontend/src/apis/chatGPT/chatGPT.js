b387448804bf0871b2c50569ed87407e1a48dd3e
YA-shiKa <myashika2005@gmail.com>
2025-07-22 16:47:50 +0530

    fix

--- mern-ai-frontend/src/apis/chatGPT/chatGPT.js
+++ mern-ai-frontend/src/apis/chatGPT/chatGPT.js
@@ -1,5 +1,7 @@
-import axios from "axios";
 
+
+import axios from "axios";
+const API = import.meta.env.VITE_API_BASE_URL;
 export const generateContentAPI = async (userPrompt) => {
   const response = await axios.post(
     "http://localhost:8090/api/v1/openai/generate-content",
